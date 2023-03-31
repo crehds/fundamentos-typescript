1d60b6cc8a972dc841eafcadce46d7685e319cb6
Carlos Rodriguez <carlos15erh@gmail.com>
2023-03-30 23:36:20 -0500

    the return of functions in TS

---
+++ src/13-return-functions.ts
@@ -0,0 +1,20 @@
+(() => {
+  const calcTotal = (prices: number[]): string => {
+    let total = 0;
+    prices.forEach((item) => {
+      total += item;
+    });
+
+    return total.toString();
+  };
+
+  // const rta = calcTotal([1, 2, 1, 1, 1]);
+  // console.log(rta);
+
+  const printTotal = (prices: number[]): void => {
+    const rta = calcTotal(prices);
+    console.log(`El total es ${rta}`);
+  };
+
+  const rta = printTotal([1, 2, 1, 1, 1]);
+})();
